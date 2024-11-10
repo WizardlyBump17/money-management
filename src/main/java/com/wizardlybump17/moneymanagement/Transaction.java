0aaa39cd8fc7f2218453bbccc80fd15df0a3d4a9
WizardlyBump17 <davicraft130@gmail.com>
2024-11-09 22:12:14 -0300

    using the right parameter types and documented

--- src/main/java/com/wizardlybump17/moneymanagement/Transaction.java
+++ src/main/java/com/wizardlybump17/moneymanagement/Transaction.java
@@ -1,10 +1,16 @@
 package com.wizardlybump17.moneymanagement;
 
+import com.wizardlybump17.moneymanagement.account.Account;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.UUID;
 
-public record Transaction(@NotNull UUID from, @NotNull UUID to, @NotNull BigDecimal amount, @NotNull Instant when) {
+/**
+ * @param from the {@link Account#id()} of the {@link Account} that is sending the money
+ * @param to the {@link Account#id()} of the {@link Account} that is receiving the money
+ * @param amount the amount of money of this transaction
+ * @param when when this transaction happened
+ */
+public record Transaction(int from, int to, @NotNull BigDecimal amount, @NotNull Instant when) {
 }

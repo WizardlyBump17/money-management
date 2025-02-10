74cadd3d43b40b3b2e3c03d01ca14e82d8f0733d
WizardlyBump17 <davicraft130@gmail.com>
2025-02-09 22:10:21 -0300

    using the Comparable instead

--- src/test/java/com/wizardlybump17/moneymanagement/test/util/AssertionUtil.java
+++ src/test/java/com/wizardlybump17/moneymanagement/test/util/AssertionUtil.java
@@ -3,15 +3,12 @@ package com.wizardlybump17.moneymanagement.test.util;
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.AssertionFailureBuilder;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 public final class AssertionUtil {
 
     private AssertionUtil() {
     }
 
-    public static void assertEquals(@Nullable BigInteger expected, @Nullable BigInteger actual) {
+    public static <T extends Comparable<T>> void assertEquals(@Nullable T expected, @Nullable T actual) {
         if (expected == null) {
             if (actual != null) {
                 AssertionFailureBuilder.assertionFailure()
@@ -39,31 +36,23 @@ public final class AssertionUtil {
         }
     }
 
-    public static void assertEquals(@Nullable BigDecimal expected, @Nullable BigDecimal actual) {
+    public static <T extends Comparable<T>> void assertNotEquals(@Nullable T expected, @Nullable T actual) {
         if (expected == null) {
-            if (actual != null) {
+            if (actual == null) {
                 AssertionFailureBuilder.assertionFailure()
-                        .expected(null)
-                        .actual(actual)
+                        .reason("expected: not equal but was: <null>")
                         .buildAndThrow();
             }
             return;
         }
 
-        if (actual == null) {
-            AssertionFailureBuilder.assertionFailure()
-                    .expected(expected)
-                    .actual(null)
-                    .buildAndThrow();
-            return;
-        }
-
-        int comparison = expected.compareTo(actual);
-        if (comparison != 0) {
-            AssertionFailureBuilder.assertionFailure()
-                    .expected(expected)
-                    .actual(actual)
-                    .buildAndThrow();
+        if (actual != null) {
+            int comparison = expected.compareTo(actual);
+            if (comparison == 0) {
+                AssertionFailureBuilder.assertionFailure()
+                        .reason("expected: not equal but was: <" + actual + ">")
+                        .buildAndThrow();
+            }
         }
     }
 }

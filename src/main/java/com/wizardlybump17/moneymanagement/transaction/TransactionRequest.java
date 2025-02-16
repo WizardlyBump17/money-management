004e7d6b352943e3a05899d83a5cef19f805512f
WizardlyBump17 <davicraft130@gmail.com>
2025-02-16 20:39:46 -0300

    renamed to TransactionRequest

--- src/main/java/com/wizardlybump17/moneymanagement/transaction/TransactionRequest.java
+++ src/main/java/com/wizardlybump17/moneymanagement/transaction/TransactionRequest.java
@@ -8,11 +8,11 @@ import java.time.Instant;
 import java.util.UUID;
 
 /**
- * @param id the id of this transaction
+ * @param id the id of this request
  * @param from the {@link Account#id()} of the {@link Account} that is sending the money
  * @param to the {@link Account#id()} of the {@link Account} that is receiving the money
- * @param amount the amount of money of this transaction
- * @param when when this transaction happened
+ * @param amount the amount of money of this request
+ * @param when when this request happened
  */
-public record Transaction(@NotNull UUID id, @NotNull UUID from, @NotNull UUID to, @NotNull BigDecimal amount, @NotNull Instant when) {
+public record TransactionRequest(@NotNull UUID id, @NotNull UUID from, @NotNull UUID to, @NotNull BigDecimal amount, @NotNull Instant when) {
 }
